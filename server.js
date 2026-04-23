f7dcc77e24209cf6c71b065deaa8630b6d367918
HaiDangPPK <skyprotect@gmail.com>
2026-04-23 10:23:16 +0700

    fi

--- server.js
+++ server.js
@@ -9,15 +9,18 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// Cấu hình Axios đặc biệt: Bỏ qua kiểm tra chứng chỉ SSL khắt khe để truy cập mượt mà các trang báo nhà nước
-// Cấu hình Axios đặc biệt: Bổ sung Header giả mạo IP Việt Nam
 const axiosClient = axios.create({
     httpsAgent: new https.Agent({ rejectUnauthorized: false }),
     headers: { 
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36',
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
         'Accept-Language': 'vi-VN,vi;q=0.9,en-US;q=0.8,en;q=0.7',
-        'X-Forwarded-For': '113.190.232.115', // Giả mạo 1 IP của Việt Nam
-        'X-Real-IP': '113.190.232.115'
+        'Referer': 'https://www.google.com/', // Báo rất thích header này
+        'Sec-Fetch-Dest': 'document',
+        'Sec-Fetch-Mode': 'navigate',
+        'Sec-Fetch-Site': 'none',
+        'Upgrade-Insecure-Requests': '1',
+        'X-Forwarded-For': '113.190.232.115'
     }
 });
 
